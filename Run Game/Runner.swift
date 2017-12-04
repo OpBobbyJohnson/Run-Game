7620ecc867370dcd377a73cb44641abbbb5f0229
Dylan Martin <dylanmartin@Dylans-Mac.local>
2017-12-04 13:16:44 -0800

    base

---
+++ Run Game/Runner.swift
@@ -0,0 +1,13 @@
+//
+//  Runner.swift
+//  Run Game
+//
+//  Created by Dylan Martin on 12/4/17.
+//  Copyright © 2017 Dylan Martin. All rights reserved.
+//
+
+import Foundation
+var isAlive = false;
+func died(){
+    isAlive = true;
+}
